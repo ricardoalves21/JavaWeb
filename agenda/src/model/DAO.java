16345ddd08e248785ada0745d1972f5d238fef1a
Ricardo Alves <dev.rical21@gmail.com>
2021-06-10 14:22:59 -0300

    Commit Java-Web 10-06-2021 14:22

--- agenda/src/model/DAO.java
+++ agenda/src/model/DAO.java
@@ -1,5 +1,28 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	
+	/** Módulo de conexão **/
+	
+	// Parâmetros de conexão
+		private String driver = "com.mysql.cj.jdbc.Driver";
+		private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+		private String user = "root";
+		private String password = "romanos8";
+				
+	// Métodos de conexão
+		private Connection conectar() {
+			Connection con = null;			
+			try {
+				Class.forName(driver);
+				con = DriverManager.getConnection(url, user, password);
+				return con;
+			} catch (Exception e) {
+				System.out.println(e);
+				return null;
+			}
+		}
 }
