cb0f1facd7b6443a4dd466662ec420117870a7e3
Ricardo Alves <dev.rical21@gmail.com>
2021-06-15 13:55:26 -0300

    Commit JavaWeb 15-06-2021 13:55

--- agenda/src/controller/Controller.java
+++ agenda/src/controller/Controller.java
@@ -1,11 +1,14 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import model.DAO;
 import model.JavaBeans;
 
@@ -40,7 +43,8 @@ public class Controller extends HttpServlet {
 // Listar contatos
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.sendRedirect("agenda.jsp");
+		// Criando um objeto que irá receber os dados JavaBeans
+		ArrayList<JavaBeans> lista = dao.listarContatos();
 	}
 
 //Novo contatos
