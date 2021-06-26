d88bf5045a2f262837784a8313a417060213df32
Ricardo Alves <dev.rical21@gmail.com>
2021-06-26 12:28:53 -0300

    Commit JavaWeb projeto api-rest-2 26-06-2021 12:28

--- api-rest-2/src/main/java/com/ricardo/apirest2/model/Cliente.java
+++ api-rest-2/src/main/java/com/ricardo/apirest2/model/Cliente.java
@@ -3,16 +3,23 @@ package com.ricardo.apirest2.model;
 import javax.persistence.*;
 import java.util.Objects;
 
-@Entity  // Esta anotação @Entity diz que esta classe faz parte de uma entidade, ou seja, uma tebela no banco de dados.
+@Entity
+// Esta anotação @Entity diz que esta classe faz parte de uma entidade, ou seja, um objeto (tebela) no banco de dados.
+
 public class Cliente {
 
-    @Id  // Esta anotação @Id diz que esta propriedade é um campo com id, ou seja, uma chave primária da tabela.
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Geração automática de valores pelo próprio BD (auto incremento).
+    @Id
+    // Esta anotação @Id diz que esta propriedade é um campo com id, ou seja, uma chave primária da tabela.
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Esta anotação @GeneratedValue diz que a geração desse campo será automática e realizada pelo próprio BD (auto incremento).
     private Long id;  // Esta propriedade possui a referência de id em uma coluna da tabela Cliente.
 
-    @Column(nullable = false)  // A anotação @Column diz que é uma coluna da tabela Cliente e o campo não pode ser null, por isso nullable = false.
+    @Column(nullable = false)
+    // A anotação @Column diz que é uma coluna da tabela Cliente e o campo não pode ser null, por isso nullable = false.
     private String nome;
 
+    // MÉTODOS GETTERS E SETTERS
     public Long getId() {
         return id;
     }
