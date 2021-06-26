7d527d1c14da12ceab3abc732ebf0ece6b5fbac6
Ricardo Alves <dev.rical21@gmail.com>
2021-06-25 22:25:57 -0300

    Commit JavaWeb atualização no projeto api_rest_1 25-06-2021 22:25

--- projeto_rest_api/src/main/java/ricardo/rest/api/repository/UsuarioRepository.java
+++ projeto_rest_api/src/main/java/ricardo/rest/api/repository/UsuarioRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import ricardo.rest.api.model.UsuarioModel;
 
 
-public interface UsuarioRepository extends CrudRepository <UsuarioModel, Integer> { // Classe que possui métodos pré-programados com as principais funcionalidades
+public interface UsuarioRepository extends CrudRepository <UsuarioModel, Integer> { // Esta Classe possui métodos pré-programados com as principais funcionalidades
+
+    /* A interface CrudRepository fornece métodos para operações CRUD, permitindo criar, ler, atualizar e excluir registros sem precisar definir seus próprios métodos. */
 
     /* Aqui estamos extendendo a classe à classe 'CrudRepository' e identificando a classe que possui o
     modelo de dados (UsuarioModel) e também o tipo (Integer) do 'id' dessa classe */
